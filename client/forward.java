db2c9ffa441b8fb773325e407e21f9f9977816a9
Michael Rapaport <rapaport1995@gmail.com>
2016-03-06 16:52:57 -0500

    Added files via upload

---
+++ client/forward.java
@@ -0,0 +1,31 @@
+package client;
+import java.io.IOException;
+
+import common.*;
+
+//Alex Prugh
+
+public class forward extends ClientCommand {
+	private String toClient;
+	private String message;
+	
+	public forward(String str, ChatClient1 client) {
+		super(str.split(" ")[0], client);
+		toClient = str.substring(0, str.indexOf(" "));
+		str = str.substring(str.indexOf(" ") + 1);
+		message = client.getId() + "> forwarded: " + str;
+	}
+	
+	@Override
+	public void doCommand() {
+		// TODO Auto-generated method stud
+		try {
+			getClient().sendToServer(new ServerForwardMessageHandler(message, toClient));
+			getClient().clientUI().display("Message forwarded.");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			// Do I add anything here?
+		}
+	}
+	
+}
